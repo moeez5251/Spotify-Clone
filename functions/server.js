d38ad787ceca9a03e2005cf23a6abfdfd0e39642
moeez5251 <moeez66656@gmail.com>
2024-09-14 17:07:20 +0500

    Server Added

---
+++ functions/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const serverless = require('serverless-http');
+const fs = require("fs");
+const path = require('path');
+const app = express()
+function readDirectoryRecursive(dir) {
+    let results = [];
+    const files = fs.readdirSync(dir);
+
+    files.forEach((file) => {
+        const fullPath = path.join(dir, file);
+        const stat = fs.statSync(fullPath);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(readDirectoryRecursive(fullPath));
+        } else {
+            results.push(fullPath);
+        }
+    });
+
+    return results;
+}
+app.get('/', (req, res) => {
+    const directoryPath = ('assets/songs');
+        const files = readDirectoryRecursive(directoryPath);
+        res.json(files);
+  
+})
+module.exports.handler = serverless(app);
